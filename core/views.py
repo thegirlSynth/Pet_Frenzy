1075e3ec01bcd884eb0ca68ce166603ee8b77ee6
The Cynthia Uche <cynthianuchepro@gmail.com>
2023-07-05 03:35:49 +0100

    Complete homepage

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage_view(request):
-    return render(request, "core/index.html")
+    return render(request, "core/home.html")

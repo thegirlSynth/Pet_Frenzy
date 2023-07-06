e56b2fecb8281285f0cc95ffbb5bbdbd17d173e0
The Cynthia Uche <cynthianuchepro@gmail.com>
2023-07-06 01:27:57 +0100

    Classify pets by categories

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import homepage_view, CategoryView
+from .views import homepage_view, CategoryView, CategoryName, PetDetails
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("", homepage_view),
     path("category/<slug:value>", CategoryView.as_view(), name="category"),
+    path("category-name/<value>", CategoryName.as_view(), name="category-name"),
+    path("pet-details/<int:num>", PetDetails.as_view(), name="pet-details"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -23,6 +23,14 @@ class CategoryView(View):
         return render(request, "core/category.html", locals())
 
 
+class CategoryName(View):
+    def get(self, request, value):
+        pets = Pet.objects.filter(name=value)
+        names = Pet.objects.filter(category=pets[0].category).values("name")
+        return render(request, "core/category.html", locals())
+
+
 class PetDetails(View):
-    def get(self, request):
+    def get(self, request, num):
+        pet = Pet.objects.get(pk=num)
         return render(request, "core/details.html", locals())
